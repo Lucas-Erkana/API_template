9658525bff7a92fe74d4f2c6b29b408e45725f86
Lucas David Erkana <lderkana21x@gmail.com>
2023-11-17 12:35:51 +0200

    Encrypt password

--- models/users.js
+++ models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
@@ -14,6 +15,16 @@ const UserSchema = new Schema({
         type: String,
         required: true,
     }
+});
+
+// Pre-save hook to hash password
+UserSchema.pre('save', async function(next) {
+    // Check if the password field is modified
+    if (this.isModified('password')) {
+        // Hash the password and replace it
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+    next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
