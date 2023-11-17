4ff493a3107bcbfcdd3879e3d0f6a13728a49929
Lucas David Erkana <lderkana21x@gmail.com>
2023-11-17 09:35:17 +0200

    Add user on API

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,27 +3,20 @@ const express = require('express');
 const router = express.Router(); // Create a router object
 const User = require('../models/users'); // Import the User model
 
+// Middleware to parse JSON data
+router.use(express.json());
+
 // Route to get the root URL
 router.get('/', (req, res) => {
     res.send({username: 'User'});
 });
 
-// Route to add users
-router.get('/add-user', async (req, res) => {
+// Route to add a new user (updated to handle POST request)
+router.post('/users', async (req, res) => {
     try {
-        await User.insertMany([
-            {
-                username: "peter",
-                email: "peter@gmail.com",
-                password: "123456",
-            },
-            {
-                username: "max",
-                email: "max@gmail.com",
-                password: "123456",
-            }
-        ]);
-        res.send('User added...');
+        const newUser = new User(req.body); // Create a new user with the request body data
+        await newUser.save(); // Save the new user
+        res.status(201).send('User added successfully');
     } catch (error) {
         console.error("Error:", error);
         res.status(500).send("Error adding user");
