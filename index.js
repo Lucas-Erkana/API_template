c4ef07bd3434355ffe742feebb1f4dc24c7a5c9f
Lucas David Erkana <lderkana21x@gmail.com>
2023-11-17 09:08:43 +0200

    remove code from index.js, then update with imports of userRoutes

--- index.js
+++ index.js
@@ -1,60 +1,30 @@
+// Import required modules
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const User = require('./models/users');
+const userRoutes = require('./routes/userRoutes'); // Import user routes
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Connect to MongoDB
 mongoose.set('strictQuery', false);
-const connectDB = async ()=> {
-    try{
+const connectDB = async () => {
+    try {
         const conn = await mongoose.connect(process.env.MONGO_URL);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.log(error);
+        console.error(error);
         process.exit(1);
     }
+};
 
-}
+// Use userRoutes for handling user-related paths
+app.use('/', userRoutes);
 
-app.get('/', (req,res)=>{
-    res.send({username: 'User'});
-})
-
-app.get('/add-user', async (req,res) =>{
-    try {
-
-        await User.insertMany([
-            {
-                username: "lucas",
-                email: "erkanalucas@gmail.com",
-                password: "123456",
-            },
-            {
-                username: "david",
-                email: "daviderkana@gmail.com",
-                password: "123456",
-            }
-        ])
-        res.send('User added...')
-    }catch (error) {
-        console.log("err", + error);
-    }
-})
-
-app.get('/users', async (req,res) =>{
-    const user = await User.find();
-    if (user) {
-        res.json(user)
-    } else {
-        res.send("Something went wrong.")
-    }
-
-})
-
-connectDB().then(()=>{
-    app.listen(PORT, ()=>{
-        console.log(`Listening on port ${PORT}`)
-    })
+// Connect to the database and start the server
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`);
+    });
 });
