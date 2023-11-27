c2f8b0d2b3a7549ab24c0eb06d698adaa371012a
Lucas David Erkana <lderkana21x@gmail.com>
2023-11-27 17:38:57 +0200

    decrypt data

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ const recipeRoutes = require('./routes/recipeRoutes'); // Import recipe routes
 const cors = require('cors');
 
 //include the Access-Control-Allow-Origin header in its responses.
-const allowedOrigins = ['http://127.0.0.1:5500', 'https://code-alpha-recipe-book.vercel.app/'];
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200
-};
+// const allowedOrigins = ['http://127.0.0.1:5500', 'https://code-alpha-recipe-book.vercel.app/'];
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     optionsSuccessStatus: 200
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 
 const app = express();
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.use(cors());
 // Connect to MongoDB
 mongoose.set('strictQuery', false);
 const connectDB = async () => {
